49818b14795a68a686c1ff1b641240709f8f5545
Sangwoo Oh <sangwoooh1013@gmail.com>
2025-08-30 11:47:39 +0900

    add: ヘッダー、フッター追加

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -1,19 +1,17 @@
-<html>
-<head>
-    <title>Welcome</title>
-</head>
-<body>
-    <h1>Welcome to Laravel</h1>
-    <a href="{{ url('/google/redirect') }}">
-        Google Calendar Login
-    </a>
-    @if(isset($calendarItems))
-        <h2>Your Calendar Items:</h2>
-        <ul>
-            @foreach($calendarItems as $item)
-                <li>{{ $item->getSummary() }}</li>
-            @endforeach
-        </ul>
+@component('components.header')
+@endcomponent
+    <h1>Welcome to Reflection Helper</h1>
+    @if(session('google_token'))
+        @if(isset($calendarItems))
+            <h2>Your Calendar Items:</h2>
+            <ul>
+                @foreach($calendarItems as $item)
+                    <li>{{ $item->getSummary() }}</li>
+                @endforeach
+            </ul>
+        @endif
+    @else
+        <p>Please log in to view your calendar items.</p>
     @endif
-</body>
-</html>
+@component('components.footer')
+@endcomponent
