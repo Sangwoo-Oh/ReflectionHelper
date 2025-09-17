65dbdbcac303a9d5d282fb0db1967de6fdad4ed9
Sangwoo Oh <epscag@inf.elte.hu>
2025-09-17 08:27:12 +0200

    style: 集計画面のフォームをbootstrap化

--- backend/resources/views/dashboard.blade.php
+++ backend/resources/views/dashboard.blade.php
@@ -5,20 +5,28 @@
         <h2>カレンダー集計:</h2>
         <form action="{{ route('aggregate') }}" method="POST">
             @csrf
-            <label>フリーワード:</label>
-            <input type="text" name="freeword" value="{{ old('freeword') }}">
-            <label>キーワード:</label>
-            <select name="keyword">
-                <option value="">選択してください</option>
-                @foreach ($keywords as $keyword)
+            <div class="mb-3">
+                <label for="freeword" class="form-label">フリーワード</label>
+                <input type="text" class="form-control" id="freeword" name="freeword" value="{{ old('freeword') }}">
+            </div>
+            <div class="mb-3">
+                <label for="keyword" class="form-label">キーワード</label>
+                <select class="form-select" name="keyword">
+                    <option value="">選択してください</option>
+                    @foreach ($keywords as $keyword)
                     <option value="{{ $keyword->id }}" {{ old('keyword') == $keyword->id ? 'selected' : '' }}>{{ $keyword->keyword }}</option>
-                @endforeach
-            </select>
-            <label>開始日:</label>
-            <input type="date" name="start_date" value="{{ old('start_date') }}">
-            <label>終了日:</label>
-            <input type="date" name="end_date" value="{{ old('end_date') }}">
-            <button type="submit">集計</button>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="start_date" class="form-label">開始日</label>
+                <input type="date" class="form-control" id="start_date" name="start_date" value="{{ old('start_date') }}">
+            </div>
+            <div class="mb-3">
+                <label for="end_date" class="form-label">終了日</label>
+                <input type="date" class="form-control" id="end_date" name="end_date" value="{{ old('end_date') }}">
+            </div>
+            <button type="submit" class="btn btn-primary">集計</button>
         </form>
         @if (session('events'))
         <ul>
