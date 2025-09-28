8c3ab11c9e74b110201545c7d42491d4989422b6
Sangwoo Oh <epscag@inf.elte.hu>
2025-09-28 17:54:15 +0200

    style; viewport

--- backend/resources/views/components/header.blade.php
+++ backend/resources/views/components/header.blade.php
@@ -1,7 +1,8 @@
 <html>
     <head>
         <title>@yield('title')@if (!request()->is('/')) - @endifシューカレ</title>
-
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head> 
     <body>
