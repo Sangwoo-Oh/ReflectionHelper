52335c8a8804f1eb1116e906e1aa6ef40b530cbd
Sangwoo Oh <epscag@inf.elte.hu>
2025-09-28 16:36:18 +0200

    bootstrapをインストール

--- backend/resources/views/components/header.blade.php
+++ backend/resources/views/components/header.blade.php
@@ -1,8 +1,8 @@
 <html>
     <head>
         <title>@yield('title')@if (!request()->is('/')) - @endifシューカレ</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head> 
     <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary mb-3">
